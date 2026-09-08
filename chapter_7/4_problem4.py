ffd98952161edc2c797eb4f13d491ede48adf6da
Gyanu Giri <gyanugiri@reyan-MacBook-Air.local>
2026-09-08 18:26:02 +0545

    fix:changed in file

--- chapter_7/4_problem4.py
+++ chapter_7/4_problem4.py
@@ -1,43 +1,37 @@
-# to find the number is positive negative or zero
-# wheather i can vote or not
-# user will enter the number and i need all the multiplication table of it 
+########### write a code wheather i can vote or not
+n=int(input("Enter your age:"))
+if(n==0):
+    print("invalid age.")
+elif(n>=18):
+    print("You are allowed to vote.")
+
+elif (n<18):
+    print("you are not allowed to vote.")
+
+else:
+    print("invalid age.")
 
 
 
-# n=int(input("Enter your age:"))
-
-
-# if(n==0):
-#     print("invalid age.")
-# elif(n>=18):
-#     print("You are allowed to vote.")
-
-# elif (n<18):
-#     print("you are not allowed to vote.")
-
-# else:
-#     print("invalid age.")
-
-
-
-# n=int(input("Enter a number:"))
-
-# if(n==0):
-#     print("Number is zero.")
-
-# elif (n>0):
-#     print("Number is positive.")
-
-# else:
-#     print("Number is negative.")
+###########  to find the number is positive negative or zero
+n=int(input("Enter a number:"))
+if(n==0):
+    print("Number is zero.")
+elif (n>0):
+    print("Number is positive.")
+else:
+    print("Number is negative.")
 
 
 
 
-# n=int(input("Enter a number:"))
+############# user will enter the number and i need all the multiplication table of it 
+n=int(input("Enter a number:"))
+for i in range(1,11):
+    print(f"{n}x{i} = {n * i}")
 
-# for i in range(1,11):
-#     print(f"{n}x{i} = {n * i}")
+
+    # or by using while loop
 
 n=int(input("Enter a number:"))
 i=1
